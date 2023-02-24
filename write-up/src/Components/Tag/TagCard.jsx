1f6176726ad323dfd2a3bf27dc2209189789ff87
prudentelias1024 <ogundipeoluwabukola2003@gmail.com>
2023-02-24 18:59:36 +0100

    Update TagCard.jsx
    Tag title is set to dynamic display

--- write-up/src/Components/Tag/TagCard.jsx
+++ write-up/src/Components/Tag/TagCard.jsx
@@ -22,10 +22,10 @@ useEffect(() => {
 
     return (
         <div>
-            <div className='  border border-t-[1em] bg-white  flex flex-row justify-between border-t-red-500 p-[1em] leading-8 lg:rounded-xl m-auto  lg:w-[70%] '>
-           <p className='font-[Mulish] text-3xl font-bold'>Inventive Thinking</p>
+            <div className='  border border-t-[1em] bg-white  flex  flex-col  text-center lg:flex-row justify-between border-t-red-500 p-[1em] leading-8 lg:rounded-xl m-auto  lg:w-[70%] '>
+           <p className='font-[Mulish] text-3xl font-bold capitalize'>{name}</p>
              
-          <button className='text-red-500 rounded-full w-[13em] h-[3em] px-6 border-2 ml-3 mt-[1.5em] font-bold border-red-500 mb-4 lg:text-lg lg:mt-[-.25em]'>Follow</button>
+          <button className='text-red-500 rounded-full w-[13em] h-[3em] m-auto px-6 border-2 lg:ml-3 mt-[1.5em] font-bold border-red-500 lg:mb-4 lg:text-lg lg:mt-[-.25em]'>Follow</button>
         </div>
         <div className="flex flex-col  m-auto  gap-8 pt-[9em] lg:pt-[1em] lg:absolute lg:top-40 lg:left-[45em]">
 
