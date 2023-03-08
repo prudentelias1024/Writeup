bc473419d9f30ccc1e0ab383ccfbf843dd79cbd6
prudentelias1024 <ogundipeoluwabukola2003@gmail.com>
2023-03-08 18:09:49 +0100

    Update AuthorInfo.jsx
    All User Post Component now shows as You if it's the author that is logged in instead of displaying their full name

--- write-up/src/Components/Post/AuthorInfo.jsx
+++ write-up/src/Components/Post/AuthorInfo.jsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
 import { useEffect } from "react";
+import { useSelector } from "react-redux";
 import { time } from "../../time";
 export default function AuthorInfo({author,timestamp}){
-    let [timePosted,setTimePosted] = useState()
+    const {user} = useSelector(state=>state)
+    const [timePosted,setTimePosted] = useState()
     useEffect(() => {
         setInterval(() => {
             setTimePosted(time(timestamp))  
@@ -14,7 +16,7 @@ export default function AuthorInfo({author,timestamp}){
         <img src={author.public_picture} className='w-[2.5em] h-[2.5em] lg:w-[3em] lg:h-[3em] rounded-full object-cover' /> 
 
         <div className=''>
-                <p className="author_title  text-md font-extrabold  text-[#171717] lg:text-xl font-[Montserrat]"> {author.name}</p>
+                <p className="author_title  text-md font-extrabold  text-[#171717] lg:text-xl font-[Montserrat]"> {author.name !== user.name ?author.name: 'You'}</p>
                 <p className="font-medium text-xs lg:mt-[.125em] lg:text-sm -mt-[.25em] text-[#717171] font-[montserrat]">Posted on {timePosted}</p>
                 </div>
         </div>
