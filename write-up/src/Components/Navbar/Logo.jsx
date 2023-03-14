49e8f1a1ac9b25f4004ab3a672096cd96fa2fb0d
prudentelias1024 <ogundipeoluwabukola2003@gmail.com>
2023-03-14 18:34:49 +0100

    Logo Updated

--- write-up/src/Components/Navbar/Logo.jsx
+++ write-up/src/Components/Navbar/Logo.jsx
@@ -1,10 +1,10 @@
 import { Link } from "react-router-dom";
-
+import Inkup from "../inkup.png";
 export default function Logo() {
     return(
     <div className=" mt-[-.5em] py-[.5em] lg:ml-[1em] flex">
-     
-        <Link to='/' className="font-[Pacifico] ml-3 mt-5 mb-2 w-[8em] text-2xl lg:text-3xl font-extrabold">Ink Up</Link>
+       <img className="w-[3em] h-[3em] mt-[1em] ml-[1em]" src={Inkup} alt="Inkup"/>
+        <Link to='/' className="font-[Pacifico] hidden lg:block ml-3 mt-5 mb-2 w-[8em]  text-2xl lg:text-3xl font-extrabold">Ink Up</Link>
     </div>
     );
 }
