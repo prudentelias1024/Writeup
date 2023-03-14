d01d55cf294e7a4fab6faebb7d830ed2cc0bacc4
prudentelias1024 <ogundipeoluwabukola2003@gmail.com>
2023-03-14 17:52:49 +0100

    Update App.js

--- write-up/src/App.js
+++ write-up/src/App.js
@@ -56,7 +56,7 @@ function App() {
           URL = "http://localhost:5000"
                  
         }
-       console.log|(URL)
+       console.log(URL)
         getPosts();
         getBookmarkedPosts();
         if (localStorage.getItem('token') !== undefined) {
