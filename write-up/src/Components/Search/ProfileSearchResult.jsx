e3ebd185b4fe23a07b2fb8780b5ab0ae1f8d2da3
prudentelias1024 <ogundipeoluwabukola2003@gmail.com>
2023-03-15 17:09:38 +0100

    Update ProfileSearchResult.jsx

--- write-up/src/Components/Search/ProfileSearchResult.jsx
+++ write-up/src/Components/Search/ProfileSearchResult.jsx
@@ -10,9 +10,9 @@ const ProfileSearchResult = ({people}) => {
                 <img className='h-[4em] w-[4em]  rounded-full mr-[1em]' src={people.public_picture} alt="" />
                 <div className="flex flex-col  ">
                     <p className="font-[Mulish] w-fit mt-[.5em] text-xl font-bold">{user == null? 
-                    <p className="font-[Mulish]  text-ellipsis h-[35px] w-[310px] overflow-hidden whitespace-nowrap  lg:w-3/4"></p> :
+                   people.name :
                     people.name == user.name? 'You' : people.name}</p>
-                    <p className="font-[Mulish]  text-ellipsis h-[35px] w-[310px] overflow-hidden whitespace-nowrap  lg:w-3/4"></p>
+                    <p className="font-[Mulish]  text-ellipsis h-[35px] w-[310px] overflow-hidden whitespace-nowrap  lg:w-3/4">{peeople.bio}</p>
                 </div>
                 {user == null ?
               (<button  className='font-[Mulish] lg:m-auto text-white w-[100%] rounded-md mt-[-.5em] bg-green-500 lg:bg-white lg:text-green-500 px-[1em] lg:mt-[0em] lg:ml-[1em] lg:w-[7em] h-[3em] p-1.5 lg:rounded-full align-middle lg:hover:border-green-500 lg:hover:border'>Follow</button>) : 
