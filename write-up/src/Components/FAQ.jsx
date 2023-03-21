e562e02e35ece4353b8cdabf591e9d38d9f91b93
prudentelias1024 <ogundipeoluwabukola2003@gmail.com>
2023-03-21 15:05:01 +0100

    Email and FAQ Updated

--- write-up/src/Components/FAQ.jsx
+++ write-up/src/Components/FAQ.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import NavBar from './NavBar';
 
 const FAQ = () => {
     return (
+        <>
+        
         <div>
-            
         </div>
+        <NavBar/>
+
+        <div className="faqs top-32 relative flex flex-row bg-white font-[Outfit] font-bold text-black">
+          <div className='flex text-2xl flex-row m-auto gap-[.25em] p-[1em]'>
+          <p className='text-black'>Frequently </p>
+          <p className='text-pink-500 font-[Maven] '>Asked Questions </p>
+          </div>
+        </div>
+        </>
     );
 }
 
