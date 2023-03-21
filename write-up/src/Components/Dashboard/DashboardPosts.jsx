7ca1cb66f5cf1bf0c66e51aff01f4e8acdba496b
prudentelias1024 <ogundipeoluwabukola2003@gmail.com>
2023-03-21 18:16:33 +0100

    Update DashboardPosts.jsx

--- write-up/src/Components/Dashboard/DashboardPosts.jsx
+++ write-up/src/Components/Dashboard/DashboardPosts.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import { FaBookmark, FaEllipsisH,  FaRegBookmark,  FaRegComment, FaRegEye, FaRegHeart } from 'react-icons/fa';
 import { IoIosHeart } from 'react-icons/io';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { format } from '../../time';
 const DashboardPosts = ({post}) => {
+    const {user} = useSelector(state => state)
     return (
-        <div className="flex flex-col justify-around border rounded-xl  w-[90%] ml-4  h-fit bg-white py-2  lg:flex-row lg:w-[60em]  lg:px-4 lg:py-6 lg:ml-[4em]">
+        <div className="flex flex-col justify-around border rounded-xl  w-[90%] ml-4  h-fit bg-white py-2  lg:flex-row lg:w-[60em]  lg:px-4 lg:py-6 lg:ml-[2em]">
         <div className='flex flex-col'>
-            <Link className="text-[#3b49df] text-xl w-full px-4 leading-8 lg:text-2xl font-bold font-[Outfit]" to="/dashboard">{post.title}</Link>
+            <Link className="text-[#3b49df] text-xl w-full px-4 leading-8 lg:text-2xl font-bold font-[Outfit]" to={'/p/@'+user.username+'/'+post.postId}>{post.title}</Link>
            
             <div className="dates flex gap-3 flex-row">
-            <p className='font-bold text-[#787878] p-2 ml-2'>Published: 30 Jan</p> 
-          
-            <p className='font-bold text-[#787878] p-2 ml-2'>Edited: 30 Jan</p> 
-          
+            <p className='font-bold text-[#787878] p-2 ml-2'>Published: {format(post.createdAt)}</p> 
+          {
+            post.edited !== undefined ?            <p className='font-bold text-[#787878] p-2 ml-2'>Edited: 30 Jan</p> 
+   :''
+          }
+        
             </div>
             </div>
             <div className="impressions flex flex-row gap-3 mt-2.5 ml-5">
