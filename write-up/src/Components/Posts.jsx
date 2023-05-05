5fa27ecba6c38378f9f6c3fdccef4fdfcb0746e9
prudentelias1024 <ogundipeoluwabukola2003@gmail.com>
2023-05-05 15:40:10 +0100

    Poll and Ads Component Created

--- write-up/src/Components/Posts.jsx
+++ write-up/src/Components/Posts.jsx
@@ -1,5 +1,6 @@
 import Post from "./Post";
 import { useSelector } from "react-redux";
+import Poll from "./poll";
 export default function Posts(){
     const {posts} = useSelector(state => state)
     if (posts == null) {
@@ -8,7 +9,7 @@ export default function Posts(){
    return(
     
     <div className="flex flex-col ml-[0em] m-auto md:mr-[3em] md:ml-[5em]  gap-1 pt-[0em] w-full  lg:pt-[1em]  lg:w-[30%]   lg:ml-[10em]">
-      
+     <Poll/> 
     {
       posts.map((post) => {
          return(<Post key={post._id} post={post} />)
