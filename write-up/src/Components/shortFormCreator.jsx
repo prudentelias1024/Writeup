e1dc0e7a9687c25b9c2eb4d7a6b5b4e7e0b2f607
prudentelias1024 <119433507+prudentelias1024@users.noreply.github.com>
2023-05-09 03:12:11 +0100

    Update shortFormCreator.jsx

--- write-up/src/Components/shortFormCreator.jsx
+++ write-up/src/Components/shortFormCreator.jsx
@@ -25,17 +25,17 @@ const ShortFormCreator = () => {
     }, []);
     if( user !== null){
     return (
-            <div className='rounded-md border bg-white font-[Outfit] py-[1em] px-[.5em] flex flex-row gap-[1em] h-fit'>
+            <div className='rounded-md border bg-white font-[Outfit]lg:mt-0 mt-[2em] py-[1em] px-[.5em] flex flex-row gap-[1em] h-fit'>
             <img src={user.public_picture} alt={user.name} className="h-[3em] w-[3em] rounded-full" />  
             <div className="creator flex flex-col">
-            <ReactQuill className='w-[30em]'  ref={quillRef} modules={modules}  placeholder='Add Post Reels' theme='bubble'  style={{color: 'black', fontFamily: 'Outfit', paddingLeft: '1em',  background: "white"}} />
+            <ReactQuill className='w-[30em]'  ref={quillRef} modules={modules}  placeholder='Add Post Reels' theme='bubble'  style={{color: 'black', fontFamily: 'Outfit', paddingLeft: '1em',  background: "white", height: '100%', width: '100%'}} />
            <div className='flex flex-row justify-between '>
-             <div className="quick_tools flex flex-row gap-[1em] ml-[2em] ">
+             <div className="quick_tools flex flex-row gap-[1em] ml-[-2em] lg:ml-[2em] ">
                 <FaPoll className='text-2xl mt-[1em] text-[rgba(0,0,0,0.5)]'/>
                 <FaImage className='text-2xl mt-[1em] text-[rgba(0,0,0,0.5)]' />
             </div>
             <div>
-                <button className='font-bold bg-green-500 px-[2em] rounded-full text-white py-[.75em] mt-2'>Publish
+                <button className='font-bold bg-green-500 px-[1em] ml-[7em] lg:px-[2em] rounded-full text-white py-[.5em] lg:py-[.75em] mt-2 lg:ml-[2em] lg:ml-[2.5em]'>Publish
                 </button>
                 </div>
             </div>
