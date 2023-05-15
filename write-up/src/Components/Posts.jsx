e596d08ba71988d0e7a3d29d15838d59dcbd1bc5
prudentelias1024 <119433507+prudentelias1024@users.noreply.github.com>
2023-05-15 23:46:16 +0100

    Update Posts.jsx

--- write-up/src/Components/Posts.jsx
+++ write-up/src/Components/Posts.jsx
@@ -23,43 +23,9 @@ export default function Posts(){
       }
    return(
     <div className="flex flex-col ml-[0.5em] m-auto md:mr-[3em] md:ml-[5em]  gap-1 pt-[0em] w-full  lg:pt-[1em]  lg:w-[30%]   lg:ml-[10em]">
-     <Poll reel={{
-            coverImageURL: mock,
-            tags: ['#help', '#distress', '#heart', '#magic'],
-            likes: [],
-            comments: [],
-            bookmarks: [],
-            totalVotes: calTotalVotes([{pollname: 'Yes', votes: 10},{ pollname: 'No', votes:0},{ pollname: 'I don\'t know', votes:7 }]),
-            created: moment(moment().subtract(3)),
-            author: {username: 'elias1024', name:'Prudent Elias', public_picture:mock},
-            viewedBy: [],
-            text: 'I actually made this happened',
-            options: [{pollname: 'Yes', votes: 10},{ pollname: 'No', votes:0},{ pollname: 'I don\'t know', votes:7 }].map((option) => {
-            
-             return  {...option, percentage:Math.round((option.votes /calTotalVotes([{pollname: 'Yes', votes: 10},{ pollname: 'No', votes:0},{ pollname: 'I don\'t know', votes:7 }]) ) * 100)}
-          }),
-            reelId: 'ae313-434d-32324',
-            viewedBy: [{username: 'elias1024', name:'Prudent Elias'}]
-
-
-
-        }}/> 
+          {/* <Poll /> 
+      <ImageReel /> */}
       <ShortFormCreator/>
-      <ImageReel reel={{
-            coverImageURL: mock,
-            tags: ['#help', '#distress', '#heart', '#magic'],
-            likes: [],
-            comments: [],
-            bookmarks: [],
-            created: moment(moment().subtract(3)),
-            author: {username: 'elias1024', name:'Prudent Elias', public_picture:mock},
-            viewedBy: [],
-            text: 'I actually made this happened',
-            postId: 'ae313-434d-32324'
-
-
-
-        }}/>
     {
       posts.map((post) => {
          return(<Post key={post._id} post={post} />)
