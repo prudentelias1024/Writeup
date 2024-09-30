8cc951273ca0e668ce1bc77bc0c1b4fcee85b1b5
prudentelias1024 <119433507+prudentelias1024@users.noreply.github.com>
2024-10-01 00:13:59 +0100

    Group creator Updated (Frontend completed)
    Current user exempted from being chosen as they are automatically the admin of the group during creation

--- write-up/src/Components/Messages/GroupCreator.jsx
+++ write-up/src/Components/Messages/GroupCreator.jsx
@@ -8,10 +8,13 @@ import { MdOutlineAddAPhoto } from "react-icons/md";
 import UploadAvatar from './add_image.png'
 import { useSelector } from 'react-redux';
 import { HiBadgeCheck } from 'react-icons/hi';
+
+import BounceLoader from "react-spinners/BounceLoader";
+import { useNavigate } from 'react-router-dom';
 export default function GroupCreator() {
  
  
-
+ const navigate = useNavigate()
   const [groupImage, setGroupImage] = useState(null)
   const [isLoading, setIsLoading] = useState(false)
   const [suggestedUsers, setSuggestedUsers] = useState([])
@@ -19,7 +22,7 @@ export default function GroupCreator() {
   const [groupMemberNames, setGroupMemberNames] = useState([])
   const groupImageRef = useRef()
   const nameRef = useRef()
-  const {URL, currentUser} = useSelector(state => state)
+  const {URL, user} = useSelector(state => state)
   const openFilePicker = () => {
     groupImageRef.current.click()
   }
@@ -58,29 +61,57 @@ export default function GroupCreator() {
     let users = await (await axios.get(`${URL}/api/users`)).data
     setSuggestedUsers(users)
    }
-
-   const createGroup = async() => {
+  const createGroup = async() => {
     setIsLoading(true)
     const res =  await (await axios.post(`${URL}/api/group/conversation`, {
       participants: groupMembers,
-      admin: currentUser._id,
+      admin: user._id,
       name: nameRef.current.value,
       icon: groupImage,
 
     })).data
     
-    if(res.status ==200){
-      setIsLoading(false)
-    
-    }
+      if(res.status == 200){
+        setIsLoading(false)
+        navigate('/message')
+      
+       }
     
    }
   useEffect(() => {
     getUsers()
+    
   }, [])
   return (
+    
    <>
+      {isLoading == true ?  
+            <div className='relative ml-[-7.5em] lg:m-0 bg-white flex flex-col'>
+                
+                      <BounceLoader
+                      color="#cbcbcbeb"
+                      loading={isLoading}
+                      cssOverride={{ 
+                          width: "100%",
+                          height: "100%",
+                          marginTop: '10em',
+                          marginLeft: '35%',
+                          zIndex: 10000,
+                          paddingLeft: '4em',
+                          position: "fixed",
+                          display: "block",
+                         
+                        }}
+                        size={300}
+                        aria-label="Loading Spinner"
+                        data-testid="loader"
+                        />
+                        <p className='fixed w-[85%] ml-[50%] lg:w-full z-[1000] text-2xl mt-[20em] lg:ml-[39%] text-black font-bold font-[Outfit]'>Creating group....</p>
+                        </div>
+                    : ''
+}
     <div className=' lg:ml-[10em] w-full lg:w-full '>
+
         <div className='flex flex-col lg:ml-[7em] lg:w-full'>
         <input onChange={uploadImage} className='opacity-0' type="file" ref={groupImageRef} />
         
@@ -123,25 +154,26 @@ export default function GroupCreator() {
 
         <div  className="flex flex-col h-[20em] overflow-y-auto">
           {
-            suggestedUsers.length > 0 && suggestedUsers.map((user) => {
+            suggestedUsers.length > 0 && suggestedUsers.map((suggestedUser) => {
               return (
-                <div onClick={() => chooseUser(user._id, user.name)}  className=
-                {groupMembers.indexOf(user._id) == -1
+                <div onClick={() => chooseUser(suggestedUser._id, suggestedUser.name)}  className=
+                {groupMembers.indexOf(suggestedUser._id) == -1 &&
+                  suggestedUser.name !== user.name
                   ?
                   " result lg:w-[50%] w-[95%] mx-[2%]":
                   "opacity-50 cursor-none result lg:w-[50%] w-[95%] mx-[2%]"
                   }>
           <div className="flex flex-row ml-[.5em]  py-[1em]">
-            <img src={user.public_picture} className='h-9 w-9 rounded-full'/>
+            <img src={suggestedUser.public_picture} className='h-9 w-9 rounded-full'/>
               <div className="profile ml-[.5em] flex flex-col gap-[.25em] ">
               <div className='flex flex-row'>
-              <p className="name text-[.75em] font-[Sen] font-bold">{currentUser &&  user.name == currentUser.name? 'You':user.name}</p>
+              <p className="name text-[.75em] font-[Sen] font-bold">{user &&  suggestedUser.name == user.name? 'You':suggestedUser.name}</p>
             {
-                user.verified !== true ? '':
+                suggestedUser.verified !== true ? '':
             <HiBadgeCheck className='text-sm text-blue-500 relative top-[.25em]'/>
             }
             </div>
-            <p className="username text-[.75em] font-[Sen] text-[#9e9e9e] font-bold -mt-[.45em]">@{user.username}</p>
+            <p className="username text-[.75em] font-[Sen] text-[#9e9e9e] font-bold -mt-[.45em]">@{suggestedUser.username}</p>
         </div>
          
      </div>
